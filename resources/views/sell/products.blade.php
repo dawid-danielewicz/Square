9f5d6ba156263c8188aee38dbb1e45ae43fbda2c
Dawid Danielewicz <dawdan1x@gmail.com>
2020-05-15 13:31:51 +0200

    improved interface

--- resources/views/sell/products.blade.php
+++ resources/views/sell/products.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-8 col-xl-9">
+        <div class="col-xs-12 col-md-8 col-xl-9">
             <h1 class="h1 ml-2 my-3 inline-block mr-auto" style="color: #364F6B;">Sprzedane produkty</h1>
             <a href="{{ route('productsStats') }}" class="btn color-bg text-light mb-3 ml-2 lg:ml-12 mt-1 inline-block">Statystyki</a>
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-3 col-xl-3">
+        <div class="col-xs-12 col-md-3 col-xl-3">
             <form class="inline-block relative mt-4 mb-2 ml-2 lg:pl-12 lg:ml-12" action="{{ route('sellProductsResults') }}" method="POST">
                 @csrf
                 <input type="text" class="p-2 rounded-l-full color-txt autoSellProduct" name="name" placeholder="szukaj..." style="background-color: #F0F0F0; border: 2px #364F6B solid; outline: none;">
--- resources/views/store/showAccessories.blade.php
+++ resources/views/store/showAccessories.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-8 col-xl-9">
+        <div class="col-xs-12 col-md-8 col-xl-9">
             <h1 class="h1 ml-2 my-3 inline-block mr-auto" style="color: #364F6B;">Akcesoria - przegląd</h1>
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-3 col-xl-3">
+        <div class="col-xs-12 col-md-3 col-xl-3">
             <form class="inline-block relative mt-4 mb-2 ml-2 lg:pl-12 lg:ml-12" action="{{ route('accessoriesResults') }}" method="POST">
                 @csrf
                 <input type="text" class="p-2 rounded-l-full color-txt autoAccessory" name="name" placeholder="szukaj..." style="background-color: #F0F0F0; border: 2px #364F6B solid; outline: none;">
